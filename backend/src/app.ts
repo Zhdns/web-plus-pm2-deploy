d8054a655fcd5679a1ef255c410f037a9188fabf
Denis Oisgold <ois@Deniss-Air.lan>
2024-04-23 12:06:56 +0300

    V2.1.0

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,6 +13,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
+mongoose.set('strictQuery', true);
+
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
 app.use(express.json());
