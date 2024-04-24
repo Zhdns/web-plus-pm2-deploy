8e973d37e4fa5b45bdff08ed49a90e3a27bf166c
Denis Oisgold <ois@Deniss-Air-2.lan>
2024-04-24 17:16:07 +0300

    V3.0.1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,38 @@
 module.exports = {
-    apps : [{
-      name: 'my-app',                 
-      script: './backend/dist/app.js',           
-      instances: 1,                     
-      autorestart: true,               
-      watch: true,                     
-      max_memory_restart: '1G',         
-      env: {
-        NODE_ENV: 'development',        
-        PORT: 3000                      
+    apps: [
+      {
+        name: 'mesto-backend',
+        script: './backend/dist/app.js',
+        instances: 1,
+        autorestart: true,
+        watch: true,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development',
+          PORT: 3000
+        },
+        env_production: {
+          NODE_ENV: 'production',
+          PORT: 3000
+        }
       },
-    }]}
+      {
+        name: 'mesto-frontend',
+        script: 'npx', 
+        args: 'serve -s build -l 5000', 
+        instances: 1,
+        autorestart: true,
+        watch: true,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development',
+          PORT: 5000
+        },
+        env_production: {
+          NODE_ENV: 'production',
+          PORT: 80 
+        }
+      }
+    ]
+  };
+  
