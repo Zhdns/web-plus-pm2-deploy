84afd0f07f1189831c5774c5cb1bc8415138d2d3
Denis Oisgold <ois@Deniss-Air-2.lan>
2024-04-24 18:33:20 +0300

    V3.0.3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
         max_memory_restart: '1G',
         env: {
           NODE_ENV: 'development',
-          PORT: 5000
+          PORT: 80
         },
         env_production: {
           NODE_ENV: 'production',
